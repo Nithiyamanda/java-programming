84b7cdc7568f0bcd09bef4fb86e1033d1f786cf4
Nithiyamanda <43032301+Nithiyamanda@users.noreply.github.com>
2018-09-07 12:25:57 +0530

    Add files via upload

---
+++ division.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    int a;
+    float b,c,d,e;
+    a=23;
+    b=5.6f;
+    c=a+b;
+    d=a/c;
+    e=d;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+    
+  }
+}
