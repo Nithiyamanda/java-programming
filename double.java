4ab349ccba6715916cf7958420fa64c50ce862c0
Nithiyamanda <43032301+Nithiyamanda@users.noreply.github.com>
2018-09-07 14:55:41 +0530

    Add files via upload

---
+++ double.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    int a=10;
+    float b=5.6768f;
+    double c,d,e;
+    c=7.2567777777;
+    d=a+c;
+    e=d/c;
+ System.out.println("print the value d:"+d);
+ System.out.println("print the value e:"+e);
+  }
+}
