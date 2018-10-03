953b57aac1266ae5bc0846996ca432e423561fa8
Nithiyamanda <43032301+Nithiyamanda@users.noreply.github.com>
2018-10-03 12:13:34 +0530

    Add files via upload

---
+++ count.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+   int count = 0, 
+   num = 548;
+
+        for(; num != 0; num/=10, ++count) {   
+        }
+
+        System.out.println("output: " + count);
+    }
+}
