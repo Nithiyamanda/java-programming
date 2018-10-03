c2050192649fdbd70f5e6ec26b6c94d6f64c8357
Nithiyamanda <43032301+Nithiyamanda@users.noreply.github.com>
2018-10-03 12:00:04 +0530

    Add files via upload

---
+++ countnumber.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+   int count = 0, 
+   num = 352417851;
+
+        for(; num != 0; num/=10, ++count) {   
+        }
+
+        System.out.println("Number of digits: " + count);
+    }
+}
