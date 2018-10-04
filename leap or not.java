9198eb528bf055d42d97005477617c83f172f188
Nithiyamanda <43032301+Nithiyamanda@users.noreply.github.com>
2018-10-04 12:35:44 +0530

    Add files via upload

---
+++ leap or not.java
@@ -0,0 +1,30 @@
+class Main {
+  public static void main(String[] args) {
+   int year=2000;
+   boolean leap=false;
+   if(year % 4 == 0)
+{
+  leap = true;
+}
+else
+{
+  leap=false;
+}
+if(year % 400 == 0)
+{
+  leap=false;
+}
+else
+{
+  leap=true;
+}
+if(leap)
+{
+  System.out.println(" true ");
+}
+else
+{
+  System.out.println(" false ");
+}
+  }}
+        
