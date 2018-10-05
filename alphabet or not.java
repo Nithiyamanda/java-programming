ce7037413edcd64b7a2719774d9f7f70e946a458
Nithiyamanda <43032301+Nithiyamanda@users.noreply.github.com>
2018-10-05 11:45:01 +0530

    Add files via upload

---
+++ alphabet or not.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    char c = 'n';
+    if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    {
+    System.out.println(" Alphabet ");
+    }
+     else
+     {
+        System.out.println(" No ");
+  }
+}
+}
