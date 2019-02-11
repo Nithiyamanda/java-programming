46d049f2dfd033eba3af6f8ec6151de171238c46
Nithiyamanda <43032301+Nithiyamanda@users.noreply.github.com>
2019-02-11 14:49:16 +0530

    Add files via upload

---
+++ polindrome.java
@@ -0,0 +1,23 @@
+class Main {
+  public static void main(String[] args) 
+  {
+    int num = 121,reversedInteger = 0, remainder,
+    originalInteger;
+
+        originalInteger = num;
+
+         
+        while( num != 0 )
+        {
+            remainder = num % 10;
+            reversedInteger = reversedInteger * 10 + remainder;
+            num  /= 10;
+        }
+
+      
+        if (originalInteger == reversedInteger)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    }
+}
