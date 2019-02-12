067727dd5b7c9a71569298297d7e434573e77491
Nithiyamanda <43032301+Nithiyamanda@users.noreply.github.com>
2019-02-12 14:13:12 +0530

    Add files via upload

---
+++ first5multiples.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    int n=5;
+
+   for(int i=1; i <= 5; i++)
+        {
+          int value=n*i;
+            System.out.println(value);
+        }
+  }
+}
