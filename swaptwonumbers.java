43362c213a999577a7f1c73d0ffecaf05a5c9072
Nithiyamanda <43032301+Nithiyamanda@users.noreply.github.com>
2019-02-12 10:28:36 +0530

    Add files via upload

---
+++ swaptwonumbers.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    int a,b;
+    a=1;
+    b=3;
+    
+System.out.println("Before swapping  a: "+ a +", b: " + b);
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println("after swapping  a: "+ a +", b: " + b);
+
+
+  }
+}
+
+    
